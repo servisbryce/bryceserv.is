b3a909562b371c61d52226fd8e0e0a6dab1efc81
servisbryce <bryceservis@bryceserv.is>
2025-03-23 16:29:06 -0500

    Regenerate to fix image compatability issues.

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,11 @@ const withMDX = mdx({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+
+    unoptimized: true
+
+  },
   pageExtensions: ["ts", "tsx", "md", "mdx"],
 };
 
