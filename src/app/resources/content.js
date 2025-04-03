4ddc1c3c9f54d7af4688da6c4671eb4d668aa4d8
servisbryce <bryceservis@bryceserv.is>
2025-04-03 17:11:49 -0500

    Added resume.pdf

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -173,7 +173,7 @@ const about = {
 
 const blog = {
   label: "Blog",
-  title: "My blogs.",
+  title: "My blog.",
   description: `Read what ${person.name} has been up to recently.`,
   // Create new blog posts by adding a new .mdx file to app/blog/posts
   // All posts will be listed on the /blog route
